f9cee311c170eb78b95009a11c5f95aee2a28e7a
MathLike <benjamin.ubilla@uc.cl>
2023-05-09 22:03:12 -0400

    Actualizado

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 En este repositorio se encuentra el código fuente de los siguientes videos de MathLike (ordenados del más nuevo hasta el más antiguo):
 
+* [¡Typst en Manim y el reemplazo definitivo de LaTeX!](https://youtube.com/shorts/OHTCyHWw-ws?feature=share)
 * [[Problema 100 subs] Problema fácil de trigonometría (solo enunciado)](https://www.youtube.com/watch?v=fXnkTQC6qSw)
--- mce/intro.py
+++ mce/intro.py
@@ -1,5 +1,5 @@
 from manim import *
-from mce.logo import Logo
+from logo import Logo
 
 
 __all__ = ["Intro"]
@@ -11,4 +11,11 @@ class Intro(Scene):
         txt = Tex("MathLike")
         VGroup(logo, txt).arrange(DOWN)
         self.play(LaggedStart(SpinInFromNothing(logo, angle=PI), Write(txt), lag_ratio=0.5))
+        self.wait()
+
+
+class IntroWithoutText(Scene):
+    def construct(self):
+        logo = Logo()
+        self.play(SpinInFromNothing(logo, angle=PI))
         self.wait()
