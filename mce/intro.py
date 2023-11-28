840b8a09349948b462b7e9d71562911c9c3382b2
MathItYT <benjamin.ubilla@uc.cl>
2023-11-28 13:48:05 -0300

    Revert "Fixed bug"
    This reverts commit 82184bb4379182145005cc6b92b7fdce14a3e5f2.

--- mce/intro.py
+++ mce/intro.py
@@ -6,21 +6,16 @@ __all__ = ["Intro"]
 
 
 class Intro(Scene):
-    logo = Logo()
-
     def construct(self):
-        logo = self.logo
+        logo = Logo()
         txt = Tex("MathLike")
         VGroup(logo, txt).arrange(DOWN)
-        self.play(LaggedStart(SpinInFromNothing(
-            logo, angle=PI), Write(txt), lag_ratio=0.5))
+        self.play(LaggedStart(SpinInFromNothing(logo, angle=PI), Write(txt), lag_ratio=0.5))
         self.wait()
 
 
 class IntroWithoutText(Scene):
-    logo = Logo()
-
     def construct(self):
-        logo = self.logo
+        logo = Logo()
         self.play(SpinInFromNothing(logo, angle=PI))
         self.wait()
--- mce/logo.py
+++ mce/logo.py
@@ -5,22 +5,16 @@ LOGO_COLOR = "#2484e3"
 
 
 class Logo(VGroup):
-    def __init__(self, logo_color=None, white=None, icon=None):
-        if logo_color is None:
-            logo_color = LOGO_COLOR
-        if white is None:
-            white = WHITE
+    def __init__(self, color=LOGO_COLOR, icon=None):
         if icon is None:
-            icon = MathTex("\\sum", color=white)
-        circ = Circle(color=white).set_fill(logo_color, opacity=1)
+            icon = MathTex("\\sum", color=WHITE)
+        circ = Circle(color=WHITE).set_fill(color, opacity=1)
         super().__init__(circ, icon)
 
 
 class YouTubeLogo(Logo):
-    def __init__(self, white=None):
-        if white is None:
-            white = WHITE
+    def __init__(self):
         youtube = SVGMobject("youtube.svg").scale(0.5)
-        youtube[0].set_color(white)
+        youtube[0].set_color(WHITE)
         youtube[1].set_color(PURE_RED)
-        super().__init__(PURE_RED, white, youtube)
+        super().__init__(PURE_RED, youtube)
